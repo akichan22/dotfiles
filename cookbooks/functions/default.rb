72c1d7d3140c8fba9c13c58599f40443c01b5701
akichan <sugawara@pepabo.com>
2019-03-02 17:26:43 +0900

    ln functionにsourceを追加

--- cookbooks/functions/default.rb
+++ cookbooks/functions/default.rb
@@ -1,6 +1,8 @@
-define :ln do
-  dotfile = File.join(ENV['HOME'], params[:name])
-  link dotfile do
-    to File.expand_path("../../../config/#{params[:name]}", __FILE__)
-  end
+define :ln, source: nil do
+	source = params[:source] || params[:name]
+
+	link File.join(ENV['HOME'], params[:name]) do
+		to File.expand_path("../../../config/#{source}", __FILE__)
+		user node[:user]
+	end
 end
